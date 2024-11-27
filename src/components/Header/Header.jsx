fd4ab88063a086ff4622c6584cb4075a21751aaf
nicolettecheng <nikocheng@gmail.com>
2024-11-26 20:08:20 -0500

    import logo, link, navlink
    add skeleton code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,24 @@
 import "./Header.scss";
-import logo from "/assets/logo/wabisabi-logo.png";
-import { Link } from "react-router-dom";
+import logo from "/assets/logo/wabisabi-logo.jpeg";
+// import user from "/assets/images/user.HEIC";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
+    <header className="nav">
+      <nav className="nav__container">
         <Link to="/">
-          <img src={logo} alt="wabisabi logo" />
+          <img src={logo} alt="wabisabi logo" className="nav__logo"/>
         </Link>
+        <div className="nav__wrap-navlinks">
+          <NavLink to="/about">about</NavLink>
+          <NavLink to="/tasks">tasks</NavLink>
+          <NavLink to="/rewards">rewards</NavLink>
+        </div>
+        <div className="nav__wrap-user">
+          <p>love u,</p>
+          <div className="nav__img-user"></div>
+        </div>
       </nav>
     </header>
   );
