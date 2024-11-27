61d09ca742ebd369f2da532c92aa0017c0e965fb
nicolettecheng <nikocheng@gmail.com>
2024-11-27 13:26:53 -0500

    resize header width and height, add bg colour, style navlinks and navlink wrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.scss";
-import logo from "/assets/logo/wabisabi-logo.jpeg";
+import logo from "/assets/logo/wabisabi-text-logo.png";
 // import user from "/assets/images/user.HEIC";
 import { Link, NavLink } from "react-router-dom";
 
@@ -8,17 +8,14 @@ export default function Header() {
     <header className="nav">
       <nav className="nav__container">
         <Link to="/">
-          <img src={logo} alt="wabisabi logo" className="nav__logo"/>
+          <img src={logo} alt="wabisabi logo" className="nav__logo" />
         </Link>
         <div className="nav__wrap-navlinks">
-          <NavLink to="/about">about</NavLink>
-          <NavLink to="/tasks">tasks</NavLink>
-          <NavLink to="/rewards">rewards</NavLink>
+          <NavLink to="/about" className="nav__navlink">about</NavLink>
+          <NavLink to="/tasks" className="nav__navlink">tasks</NavLink>
+          <NavLink to="/rewards" className="nav__navlink">rewards</NavLink>
         </div>
-        <div className="nav__wrap-user">
-          <p>love u,</p>
-          <div className="nav__img-user"></div>
-        </div>
+        <div className="nav__img-user"></div>
       </nav>
     </header>
   );
