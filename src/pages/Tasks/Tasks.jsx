082efb5aec8f0eba66e514a5e11a8e4a62644498
nicolettecheng <nikocheng@gmail.com>
2024-11-28 16:11:03 -0500

    initialize Tasks component, add skeleton code including use hooks, add get request

---
+++ src/pages/Tasks/Tasks.jsx
@@ -0,0 +1,32 @@
+import TasksList from "../../components/TasksList/TasksList";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Tasks() {
+  const [taskItems, setTaskItems] = useState([]);
+
+  useEffect(() => {
+    generatetTaskItems();
+  }, []);
+
+  const generatetTaskItems = async () => {
+    try {
+      const { data } = await axios.get(`${apiUrl}/tasks`);
+      setTaskItems(data);
+      // console.log(data);
+    } catch (error) {
+      console.error("Error fetching tasks data:", error);
+    }
+  };
+
+  return (
+    <>
+      <TasksList
+        taskItems={tasksItems}
+        generatetTaskItem={generatetTaskItems}
+      />
+    </>
+  );
+}
