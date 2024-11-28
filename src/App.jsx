5ad5d38bb683019b36ed3c09309cad05d1e94f0b
nicolettecheng <nikocheng@gmail.com>
2024-11-28 15:28:52 -0500

    import home page component and add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./pages/HomePage/Home";
+import React from "react";
 import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
