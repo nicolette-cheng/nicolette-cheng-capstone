990f94eb9009b385af67c80caa63e43d73d967f2
nicolettecheng <nikocheng@gmail.com>
2024-11-30 16:12:15 -0500

    initialize file and add skeleton code

---
+++ src/components/DetailsSpecs/DetailsSpecs.jsx
@@ -0,0 +1,20 @@
+import "./DetailsSpecs.scss";
+import starIcon from "/assets/icons/shooting-star.svg";
+
+export default function DetailsSpecs({ stars, created, updated }) {
+  const formattedCreatedDate = new Date(created).toISOString().split("T")[0];
+  const formattedUpdatedDate = new Date(updated).toISOString().split("T")[0];
+
+  return (
+    <div className="details-specs">
+      <h2 className="details-specs__wrapper-icon">
+        {stars}
+        <img src={starIcon} alt="" className="details-specs__icon" />
+      </h2>
+      <div className="details-specs__wrapper-timestamps">
+        <p>created: {formattedCreatedDate}</p>
+        <p>last updated: {formattedUpdatedDate}</p>
+      </div>
+    </div>
+  );
+}
