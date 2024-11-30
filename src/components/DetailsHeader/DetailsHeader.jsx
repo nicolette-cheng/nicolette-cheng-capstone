05e77173ef3db3f2e957a02b927a1d33af5dec8a
nicolettecheng <nikocheng@gmail.com>
2024-11-30 15:13:29 -0500

    refactor code to new design

--- src/components/DetailsHeader/DetailsHeader.jsx
+++ src/components/DetailsHeader/DetailsHeader.jsx
@@ -1,27 +1,30 @@
 import "./DetailsHeader.scss";
 import { Link } from "react-router-dom";
-import { ChevronLeft, PencilIcon } from "lucide-react";
+import { ChevronLeft, PencilIcon, Trash2 } from "lucide-react";
 
-export default function DetailsHeader({ title, pathBack, pathEdit }) {
+export default function DetailsHeader({
+  title,
+  pathBack,
+  pathEdit /* , pathDelete */,
+}) {
   return (
     <div className="details-title__card">
-      <div className="details-title__wrapper">
+      <div className="details-title__wrapper-title">
         <Link to={pathBack}>
-          <img
-            src={ChevronLeft}
-            alt="back/chevron icon"
-            className="details-title__icon-back"
-          />
+          <ChevronLeft className="details-title__icon-back" />
         </Link>
         <h2 className="details-title__header">{title}</h2>
       </div>
-      <Link to={pathEdit}>
-        <img
-          src={PencilIcon}
-          alt="pencil/edit icon"
-          className="details-title__icon-edit"
-        />
-      </Link>
+      <div className="details-title__wrapper-links">
+        <Link to={pathEdit} className="details-title__link-edit">
+          <PencilIcon className="details-title__icon-edit" />
+        </Link>
+        {/* <Link to={pathDelete} className="details-title__link-delete"> */}
+        {/* ^commented out to add back in when delete functionality is added */}
+        <Link to="/" className="details-title__link-delete">
+          <Trash2 className="details-title__icon-delete" />
+        </Link>
+      </div>
     </div>
   );
 }
