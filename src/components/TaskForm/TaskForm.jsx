83c92793f2dcbb7bd39984b75604d1a24fc847a4
nicolettecheng <nikocheng@gmail.com>
2024-12-01 17:53:04 -0500

    add: validateFields, handleSubmit

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -3,6 +3,7 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import backIcon from "/assets/icons/arrow_back.svg";
+import { OctagonAlertIcon } from "lucide-react";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -33,7 +34,6 @@ export default function TaskForm() {
     {
       label: "reward associated",
       name: "reward",
-      options: ["brunch", "buy game"],
     },
   ];
 
@@ -44,14 +44,17 @@ export default function TaskForm() {
         .then((response) => {
           const taskItem = response.data;
 
+          // if reward_id is missing, find it via reward_name instead
           let reward_id = taskItem.reward_id;
 
           if (!reward_id && taskItem.reward_id) {
             const selectedReward = rewards.find(
               (reward) => reward.reward_name === taskItem.reward_name
             );
+            reward_id = selectedReward ? selectedReward.id : "";
           }
 
+          // prepopuldate form data w/existing task details
           setFormData({
             task_name: taskItem.task_name,
             description: taskItem.description,
@@ -66,15 +69,15 @@ export default function TaskForm() {
           }
         });
     }
-  }, [isEditMode, id, rewards]);
+  }, [isEditMode, id, rewards]); // dependent on "rewards" in case they are updated
 
   useEffect(() => {
     fetchRewards();
   }, []);
   const fetchRewards = async () => {
     try {
-      const { data } = axios.get(`${apiUrl}/rewards`);
-      setRewards(data);
+      const response = await axios.get(`${apiUrl}/rewards`);
+      setRewards(response.data); // sets task list as all task items
     } catch (error) {
       console.error("Error fetching rewards data:", error);
     }
@@ -82,6 +85,7 @@ export default function TaskForm() {
 
   const [errors, setErrors] = useState({});
 
+  // handles field changes and removes error message on input
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
@@ -89,11 +93,13 @@ export default function TaskForm() {
       setErrors((prev) => ({ ...prev, [name]: "" }));
     }
 
+    // if 'reward' field changes, find the reward_id by reward name
     if (name === "reward") {
       const selectedReward = rewards.find(
         (reward) => reward.reward_name === value
       );
 
+      // ensure reward_id is set correctly
       setFormData((prev) => ({
         ...prev,
         reward_id: selectedReward ? selectedReward.id : "",
@@ -101,7 +107,66 @@ export default function TaskForm() {
     }
   };
 
-  
+  // validates form fields
+  const validateFields = () => {
+    const newErrors = {};
+    if (!formData.task_name.trim()) {
+      newErrors.task_name = "Task name is required.";
+    }
+    if (!formData.description.trim()) {
+      newErrors.description = "Description is required.";
+    }
+    if (!formData.stars_required.trim()) {
+      newErrors.stars_required =
+        "Stars count is required and must be a number.";
+    }
+    if (!formData.reward.trim()) {
+      newErrors.reward = "Reward selection is required.";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  // handles form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validateFields()) {
+      try {
+        // create requestData with updated formData
+        const requestData = {
+          ...formData,
+          reward_id: formData.reward_id,
+        };
+
+        delete requestData.reward; // remove reward name from request data
+
+        const response = isEditMode
+          ? await axios.put(`${apiUrl}/tasks/${id}`, requestData)
+          : await axios.post(`${apiUrl}/tasks/`, requestData);
+
+        if (response.status === 201 || response.status === 200) {
+          setFormData({
+            task_name: "",
+            description: "",
+            stars_required: "",
+            reward: "",
+            reward_id: "",
+          });
+
+          if (isEditMode) {
+            navigate(`tasks/${id}`);
+          } else {
+            navigate("/tasks");
+          }
+        }
+      } catch (error) {
+        console.error(
+          "Error connecting to the server, please try again later."
+        );
+      }
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -111,6 +176,8 @@ export default function TaskForm() {
         </Link>
         <h1>{isEditMode ? "edit task" : "add new task"}</h1>
       </legend>
+
+
     </form>
   );
 }
