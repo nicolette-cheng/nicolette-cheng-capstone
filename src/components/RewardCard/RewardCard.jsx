515a06d1aa85a0611e3f982df6d3b0256d888cad
nicolettecheng <nikocheng@gmail.com>
2024-12-02 17:50:55 -0500

    initialize file and add skeleton code

---
+++ src/components/RewardCard/RewardCard.jsx
@@ -0,0 +1,28 @@
+import "./RewardCard.scss";
+import { Link } from "react-router-dom";
+import React from "react";
+import starIcon from "/assets/icons/shooting-star.svg";
+import caretRightIcon from "/assets/icons/caret-double-right.svg";
+
+export default function RewardCard({ reward }) {
+  return (
+    <Link to={`rewards/${reward.id}`}>
+      <div className="reward-card">
+        <h3>{reward.reward_name}</h3>
+        <div className="reward-card__stars-wrap">
+          <img
+            src={starIcon}
+            alt="star icon"
+            className="reward-card__icon-stars"
+          />
+          <p>{reward.stars_required}</p>
+        </div>
+        <img
+          src={caretRightIcon}
+          alt="arrow pointing right icon"
+          className="reward-card__icon-caret"
+        />
+      </div>
+    </Link>
+  );
+}
