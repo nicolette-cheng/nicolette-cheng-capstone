87e42055b2d5ec03c3069f41d2d3a77790470574
nicolettecheng <nikocheng@gmail.com>
2024-12-03 18:31:14 -0500

    refactor to accomodate new styling

--- src/components/QuickActionCard/QuickActionCard.jsx
+++ src/components/QuickActionCard/QuickActionCard.jsx
@@ -4,11 +4,13 @@ import "./QuickActionCard.scss";
 export default function QuickActionCard({ title, icon, description, action }) {
   return (
     <div className="quick-action__card" onClick={action}>
-      <div className="quick-action__card-title">
-        <img src={icon} alt="action icon" className="quick-action__card-icon"/>
-        <h3>{title}</h3>
+      <div className="quick-action__content">
+        <div className="quick-action__visible">
+          <img src={icon} alt={`${title} icon`} />
+          <h2 className="quick-action__title">{title}</h2>
+        </div>
+        <p className="quick-action__description">{description}</p>
       </div>
-      <p>{description}</p>
     </div>
   );
 }
