2e80a32015fc1cabb7fbf76a67243e0432c1ae90
nicolettecheng <nikocheng@gmail.com>
2024-12-04 18:46:40 -0500

    add styling and aria-label

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -6,20 +6,29 @@ import caretRightIcon from "/assets/icons/caret-double-right.svg";
 
 export default function TaskCard({ task }) {
   return (
-    <Link to={`/tasks/${task.id}`}>
+    <Link
+      to={`/tasks/${task.id}`}
+      className="task-card__link"
+      aria-label={`View task: ${task.task_name}, ${task.stars_required} stars required`}
+    >
       <div className="task-card">
-        <h3>{task.task_name}</h3>
-        <div className="task-card__stars-wrap">
+        <h2 className="task-card__title">{task.task_name}</h2>
+        <div
+          className="task-card__stars-wrap"
+          aria-label={`${task.stars_required} stars required`}
+        >
           <img
             src={starIcon}
-            alt="star icon"
+            alt=""
+            aria-hidden="true"
             className="task-card__icon-stars"
           />
-          <p>{task.stars_required}</p>
+          <p className="task-card__stars-count">{task.stars_required}</p>
         </div>
         <img
           src={caretRightIcon}
-          alt="arrow pointing right icon"
+          alt=""
+          aria-hidden="true"
           className="task-card__icon-caret"
         />
       </div>
