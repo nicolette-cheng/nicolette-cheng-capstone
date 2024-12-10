d623ef77369ca2f9c073150301577215378e0487
nicolettecheng <nikocheng@gmail.com>
2024-12-09 23:08:10 -0500

    add TaskEdit and route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home.jsx";
 import Tasks from "./pages/Tasks/Tasks.jsx";
 import TaskDetails from "./pages/TaskDetails/TaskDetails.jsx";
 import TaskAdd from "./pages/TaskAdd/TaskAdd.jsx";
+import TaskEdit from "./pages/TaskEdit/TaskEdit.jsx";
 import Rewards from "./pages/Rewards/Rewards.jsx";
 import RewardDetails from "./pages/RewardDetails/RewardDetails.jsx";
 import RewardAdd from "./pages/RewardAdd/RewardAdd.jsx";
@@ -20,6 +21,7 @@ function App() {
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/tasks/:id" element={<TaskDetails />} />
           <Route path="/tasks/add" element={<TaskAdd />} />
+          <Route path="/tasks/:id/edit" element={<TaskEdit />} />
           <Route path="/rewards" element={<Rewards />} />
           <Route path="/rewards/:id" element={<RewardDetails />} />
           <Route path="/rewards/add" element={<RewardAdd />} />
